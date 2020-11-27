2429270f2a1646f51374ecb318a4b18a1bd6959a
ange47rm <ange47rm@hotmail.com>
2020-11-27 16:32:33 +0000

    Song class tests and Guest class tests (5 in total) passed.

--- src/guest.py
+++ src/guest.py
@@ -1,2 +1,10 @@
+from src.song import Song 
+
 class Guest ():
-    pass
+    
+    def __init__ (self, name, money, Song):
+        self.name = name
+        self.money = money
+        self.Song = Song
+
+    
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,4 +4,29 @@ from src.guest import Guest
 from src.song import Song
 
 class TestGuest (unittest.TestCase):
-    pass
+    
+    def setUp (self):
+        
+        # SONGS SETUP #
+        self.song_1 = Song ("2Pac", "Dear Mama")
+        self.song_2 = Song ("The Notorious BIG", "Juicy")
+        self.song_3 = Song ("The Midnight", "Los Angeles")
+
+        # GUESTS SETUP #
+        self.guest_1 = Guest ("Angelo", 47, self.song_2)
+        self.guest_2 = Guest ("Ryan", 128, self.song_3)
+        self.guest_3 = Guest ("Sonia", 22, self.song_1)
+
+    
+    def test_guest_has_name (self):
+        self.assertEqual ("Sonia", self.guest_3.name)
+
+    def test_guest_has_money (self):
+        self.assertEqual (128, self.guest_2.money)
+        
+    def test_guest_has_fave_song(self):
+        self.assertEqual (("The Notorious BIG", "Juicy"), (self.guest_1.Song.artist, self.guest_1.Song.title))
+
+
+
+    
