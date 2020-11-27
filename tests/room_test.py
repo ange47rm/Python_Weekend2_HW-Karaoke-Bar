5065ffae4f153be264c3a4819e1d4e1a30668da3
ange47rm <ange47rm@hotmail.com>
2020-11-27 15:31:46 +0000

    Song class tests (2) passed.

---
+++ tests/room_test.py
@@ -0,0 +1,8 @@
+import unittest
+
+from src.guest import Guest 
+from src.song import Song 
+from src.room import Room 
+
+class TestRoom (unittest.TestCase):
+    pass
--- tests/song_test.py
+++ tests/song_test.py
@@ -4,4 +4,14 @@ from src.song import Song
 
 
 class TestSong (unittest.TestCase):
-    pass
+
+    def setUp (self):
+        self.song_1 = Song ("Jay Z", "Story of OJ")
+        self.song_2 = Song ("Gunship", "Tech Noir")
+        self.song_3 = Song ("The Midnight", "Los Angeles")
+
+    def test_song_has_artist (self):
+        self.assertEqual ("The Midnight", self.song_3.artist)
+
+    def test_song_has_title (self):
+        self.assertEqual ("Tech Noir", self.song_2.title)
